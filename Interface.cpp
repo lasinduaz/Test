cd7d67db2d143c312bafd3ef068690f86d452451
lasinduaz <lasindua#gmail.com>
2024-12-08 16:25:28 +0530

    Create Interface.cpp

---
+++ Interface.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+using namespace std;
+int main(){
+
+    string palace_name;
+    cout << "Enter your location :";
+    cin >> palace_name;
+
+    return 0;
+}
